ff498c0d6ed6e3a904256c5e2f3e810957981d0c
Hubert Miaz <s33124@pjwstk.edu.pl>
2026-03-14 18:43:45 +0100

    Commit2: dodanie opcji sumy i różnicy

--- Program.cs
+++ Program.cs
@@ -3,3 +3,9 @@ double liczba1 = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Podaj drugą liczbę:");
 double liczba2 = double.Parse(Console.ReadLine());
+
+double suma = liczba1 + liczba2;
+double roznica = liczba1 - liczba2;
+
+Console.WriteLine($"Suma: {suma}");
+Console.WriteLine($"Różnica: {roznica}");
